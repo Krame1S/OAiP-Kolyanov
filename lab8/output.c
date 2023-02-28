21934bfae9ae93dd2ab7988deda29405e2621ccd
Ilya Kolyanov <112816372+Krame1S@users.noreply.github.com>
2023-02-28 20:10:56 +0300

    Delete lab8 directory

--- lab8/output.c
+++
@@ -1,9 +0,0 @@
-#include "output.h"
-
-void output_string(char** string, int argc)
-{
-	for (int i = 1; i < argc; i++)
-	{
-		printf("%s\n", string[i]);
-	}
-}
--- lab8/sort.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-
-void insert_sort(char*** string, const int argc);
