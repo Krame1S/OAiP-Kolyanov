12a93c47834b82c145ea1b055bd8bf8c7b18ad29
Ilya Kolyanov <112816372+Krame1S@users.noreply.github.com>
2023-02-28 20:29:15 +0300

    Delete sem(4) directory

--- sem(4)/Lab6.2/Lab6.2/sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include "arrays.h"
-#include "input.h"
-#include "output.h"
-#include "sort.h"
-
-void shell_sort(int*** array, int* rows, int* cols);
--- sem(4)/Lab7/Lab7/Source.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-#include "task1.h"
-#include "task2.h"
-
