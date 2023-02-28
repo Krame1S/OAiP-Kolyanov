c1db5a70fc1e9f49a78b8ddac28400a92e9dcccf
Ilya Kolyanov <112816372+Krame1S@users.noreply.github.com>
2023-02-28 20:09:10 +0300

    Delete Lab5.2 directory

--- Lab5.2/input.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include "arrays.h"
-#include "output.h"
-
-void input_array_size(int* rows);
-void input_array(int* rows, int*** array);
-void input_k(int* k);
--- Lab5.2/output.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-void output_wrong_input();
-void output_enter_array_promt();
-void output_null_enter_promt();
-void output_one_enter_promt();
-void output_initial_array(int* rows, int*** array);
-void output_new_array(int* rows, int*** array);
