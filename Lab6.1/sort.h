633ac66cf506f1131d61176289fbf03742dabea7
Ilya Kolyanov <112816372+Krame1S@users.noreply.github.com>
2023-02-28 20:09:45 +0300

    Delete Lab6.1 directory

--- Lab6.1/sort.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <time.h>
-#include "array.h"
-#include "output.h"
-
-void bubble_sort(int* k, int* SIZE, int** array, int* FLAG);
-void selection_sort(int* k, int* SIZE, int** array2);
--- Lab6.1/time.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <time.h>
-#include <stdio.h>
-#include "array.h"
-
-void time_clocking_bubble_sort(int* k, int* SIZE, int** array, int* FLAG, double* bubble_sort_time);
-void time_clocking_selection_sort(int* k, int* SIZE, int** array, double* selection_sort_time);
-void time_clocking_worst_case_bubble_sort(int* k, int* SIZE, int** array, int* FLAG, double* bubble_sort_worst_case_time);
-void time_clocking_worst_case_selection_sort(int* k, int* SIZE, int** array, double* selection_sort_worst_case_time);
