0ab57e9d07d46f802f297dae733e52c84783421f
Ilya Kolyanov <112816372+Krame1S@users.noreply.github.com>
2023-02-28 20:08:48 +0300

    Delete Lab5.1 directory

--- Lab5.1/Laba5.1/input_check.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-
-void input_check_size(int *SIZE);
-void input_check_arr(int *SIZE, int* arr);
--- Lab5.1/arrays.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-
-void arrays_del_smaller_then_prew(int *SIZE, int **arr);
