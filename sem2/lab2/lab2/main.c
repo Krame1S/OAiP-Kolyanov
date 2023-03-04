7c9e6fa2c7e668381cc3978b40231d6cf809d1db
Ilya Kolyanov <112816372+Krame1S@users.noreply.github.com>
2023-03-04 12:12:47 +0300

    Update main.c

--- sem2/lab2/lab2/main.c
+++ sem2/lab2/lab2/main.c
@@ -15,9 +15,9 @@ tList* createNode(int setNum, char* setWord) {
 	tList* node = (tList*)malloc(sizeof(tList));
 
 	if (node != NULL) {
+
 		node->num = setNum;
 		node->word = setWord;
-
 
 		node->next = NULL;
 	}
@@ -25,15 +25,74 @@ tList* createNode(int setNum, char* setWord) {
 	return node;
 }
 
-int main() {
-	
-	tList* list = createNode(4, "frog");
+void pushFront(tList** list, int setNum, char* setWord, int* nodesAmount) {
 
-	while (list != NULL) {
-		printf("Word %s appears %d times in this file", list->word, list->num);
-		list = list->next;
+	tList* newNode = createNode(setNum, setWord);
+
+	newNode->next = *list;
+	*list = newNode;
+
+	(*nodesAmount)++;
+}
+
+void pushInsert(tList** list, int setNum, char* setWord, int pos, int* nodesAmount) {
+
+	if ((*nodesAmount) < pos-1) {
+		printf("error\n");
+		exit(1);
 	}
 
+	tList* newNode = createNode(setNum, setWord);
+
+	tList* temp = (*list);
+	for (int i = 0; i < (pos-2); i++)
+		temp = temp->next;
+	
+	newNode->next = temp->next;
+	temp->next = newNode;
+
+	(*nodesAmount)++;
+}
+
+void pushBack(tList** list, int setNum, char* setWord, int* nodesAmount) {
+	
+	tList* newNode = createNode(setNum, setWord);
+
+	tList* temp = (*list);
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = newNode;
+
+	(*nodesAmount)++;
+}
+
+void memoryFree(tList* usedList) {
+	tList* temp;
+
+	while (usedList) {
+		temp = usedList;
+		usedList = usedList->next;
+		free(temp);
+	}
+}
+
+int main() {
+	
+	int nodesAmount = 0;
+	tList* list = createNode(4, "BASEfrog");
+
+	pushFront(&list, 12, "front", &nodesAmount);
+	pushBack(&list, 2, "back", &nodesAmount);
+	pushInsert(&list, 44, "OOOOOOOOOOO", 3, &nodesAmount);
+
+	while (list != NULL) {
+		printf("Word %s appears %d times in this file\n", list->word, list->num);
+		list = list->next;
+	}	
+
+	memoryFree(list);
 
 	return 0;
 }
